b4434396336b7ebb4f1e3fce804212098ba8347a
Alexis <alexis.montorier@laposte.net>
2018-05-09 10:59:02 +0200

    Mise en ligne du projet de COO

---
+++ ProG/Almontorie.ProG.Model/Library.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almontorie.ProG.Model
+{
+    public class Library
+    {
+        public List<Song> ListSong { get; private set; }
+        public List<Artist> ListArtist  { get; private set; }
+        public List<Album> ListAlbum { get; private set; }
+        public List<Playlist> ListPlaylist { get; private set; }
+
+        public Time Length { get; private set; }
+
+        public Library()
+        {
+            ListSong = new List<Song>();
+            ListArtist = new List<Artist>();
+            ListAlbum = new List<Album>();
+            ListPlaylist = new List<Playlist>();
+            Length = new Time();
+        }
+
+        public void AddSong(Song Track)
+        {
+            if (ListSong.Contains(Track))
+            {
+                Console.WriteLine("Cette musique est déjà présente dans la librarie");
+                return;
+            }
+            ListSong.Add(Track);
+            Length = Length.Addition(Track.Length);
+
+            if (!ListArtist.Contains(Track.Artist))
+                AddArtist(Track.Artist);
+            ListArtist[ListArtist.IndexOf(Track.Artist)].AddSong(Track);
+
+            if (Track.Album.Name != "Unknown")
+            {
+                if (!ListAlbum.Contains(Track.Album))
+                    AddAlbum(Track.Album);
+                ListAlbum[ListAlbum.IndexOf(Track.Album)].AddSong(Track);
+            }
+        }
+
+        public void DeleteSong(Song Track)
+        {
+            ListSong.Remove(Track);
+            Length = Length.Substraction(Track.Length);
+
+            if (Track.Album.Name != "Unknown")
+            {
+                int i;
+                i = ListAlbum.IndexOf(Track.Album);
+                ListAlbum[i].DeleteSong(Track);
+            }
+
+            foreach (var playlist in ListPlaylist)
+            {
+                if (playlist.ListSong.Contains(Track))
+                    playlist.DeleteSong(Track);
+            }
+
+            int it;
+            it = ListArtist.IndexOf(Track.Artist);
+            ListArtist[it].DeleteSong(Track);
+            if (ListArtist[it].ListSong.Count == 0)
+                DeleteArtist(Track.Artist);
+
+        }
+
+        private void AddAlbum(Album Album)
+        {
+            ListAlbum.Add(Album);
+        }
+
+        public void DeleteAlbum(Album Album)
+        {
+            Album.ListSong.Clear();
+            ListAlbum.Remove(Album);
+        }
+
+        private void AddArtist(Artist Artist)
+        {
+            ListArtist.Add(Artist);
+        }
+
+        private void DeleteArtist(Artist Artist)
+        {
+            ListArtist.Remove(Artist);
+        }
+
+        public void AddPlaylist(Playlist TrackList)
+        {
+            if (ListPlaylist.Contains(TrackList))
+            {
+                Console.WriteLine("Cette playlist existe déjà");
+                return;
+            }
+            ListPlaylist.Add(TrackList);
+        }
+
+        public void DeletePlaylist(Playlist TrackList)
+        {
+            TrackList.ListSong.Clear();
+            ListPlaylist.Remove(TrackList);
+        }
+
+        
+
+    }
+}
---
+++ ProG/Almontorie.ProG.Model/Time.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almontorie.ProG.Model
+{
+    public class Time : Date   
+    {
+        public int Hour { get; private set; }
+        public int Min { get; private set; }
+        public int Second { get; private set; }
+
+        public Time(int Hour, int Min, int Second) :base() 
+        {
+            if (Hour >= 24 || Min >= 60 || Second >= 60)
+                throw new ArgumentOutOfRangeException("Les valeurs passées en paramètres sont trop grandes");
+            if (Hour < 0 || Min < 0 || Second < 0)
+                throw new ArgumentOutOfRangeException("Les valeurs passées en paramètres sont négatives");
+            this.Hour = Hour;
+            this.Min = Min;
+            this.Second = Second;
+        }
+
+        public Time(int Min, int Second) : this(0, Min, Second)
+        {
+        }
+
+        public Time() : this(0, 0, 0)
+        {
+        }
+        
+        public Time Addition(Time Time)
+        {
+            Time retour = new Time();
+            retour.Second = Second + Time.Second;
+            if (retour.Second >= 60)
+            {
+                retour.Min += 1;
+                retour.Second -= 60;
+            }
+
+            retour.Min = retour.Min + Min + Time.Min;
+            if (retour.Min >= 60)
+            {
+                retour.Hour += 1;
+                retour.Min -= 60;
+            }
+
+            retour.Hour = retour.Hour + Hour + Time.Hour;
+            if (retour.Hour >= 24)
+            {
+                retour.Day += 1;
+                retour.Hour -= 24;
+            }
+
+            retour.Day = retour.Day + Day + Time.Day;
+
+
+            return retour;
+        }
+
+        public Time Substraction(Time Time)
+        {
+            Time retour = new Time();
+
+            retour.Second = Second - Time.Second;
+            if (retour.Second < 0)
+            {
+                retour.Min -= 1;
+                retour.Second += 60;
+            }
+
+            retour.Min = retour.Min + Min - Time.Min;
+            if (retour.Min < 0)
+            {
+                retour.Hour -= 1;
+                retour.Min += 60;
+            }
+
+            retour.Hour = retour.Hour + Hour - Time.Hour;
+            if (retour.Hour < 0)
+            {
+                retour.Day -= 1;
+                retour.Hour += 24;
+            }
+
+            retour.Day = retour.Day + Day - Time.Day;
+              
+            return retour;
+        }
+
+        public override string ToString()
+        {
+            if(Day ==0 )
+                return Hour + ":" + Min + ":"+ Second;
+            return Day + "j " + Hour + ":" + Min + ":" + Second;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var time = obj as Time;
+            return time != null &&
+                   base.Equals(obj) &&
+                   Hour == time.Hour &&
+                   Min == time.Min &&
+                   Second == time.Second;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1227425067;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + Hour.GetHashCode();
+            hashCode = hashCode * -1521134295 + Min.GetHashCode();
+            hashCode = hashCode * -1521134295 + Second.GetHashCode();
+            return hashCode;
+        }
+    }
+}
