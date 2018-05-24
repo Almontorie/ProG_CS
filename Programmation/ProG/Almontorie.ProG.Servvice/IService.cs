fefb089ca33a70fcaec92c204adc1443e74df28a
Alexis <alexis.montorier@laposte.net>
2018-05-24 18:45:08 +0200

    ajout de l'interface IService

---
+++ Programmation/ProG/Almontorie.ProG.Servvice/IService.cs
@@ -0,0 +1,16 @@
+﻿using Almontorie.ProG.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almontorie.ProG.Service
+{
+    public interface IService
+    {
+        void SaveLibrary(Library myLibrary);
+
+        Library LoadLibrary();
+    }
+}
---
+++ Programmation/ProG/Almontorie.ProG.Servvice/JsonService.cs
@@ -0,0 +1,46 @@
+﻿using Almontorie.ProG.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Almontorie.ProG.Model;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Almontorie.ProG.Service
+{
+    public class JsonService : IService
+    {
+        public Library LoadLibrary()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "save.json");
+            Uri u = new Uri(path);
+            string content;
+            try
+            {
+                content = File.ReadAllText(path);
+                
+            }
+            catch (FileNotFoundException)
+            {
+                return new Library();
+            }
+
+            Library l = JsonConvert.DeserializeObject<Library>(content);
+
+            return l;
+        }
+
+        public void SaveLibrary(Library myLibrary)
+        {
+            using (StreamWriter sw = File.CreateText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "save.json")))
+            {
+                JsonConvert.SerializeObject(myLibrary);
+            }
+                
+        }
+    }
+
+}
+
