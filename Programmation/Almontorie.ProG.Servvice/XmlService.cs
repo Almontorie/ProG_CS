17fcd0b7cbd8edba9ae139e15d3688b1cb647f4b
Alexis <alexis.montorier@laposte.net>
2018-06-11 15:28:37 +0200

    ajout du setup

--- Programmation/Almontorie.ProG.Servvice/XmlService.cs
+++ Programmation/Almontorie.ProG.Servvice/XmlService.cs
@@ -56,5 +56,14 @@ namespace Almontorie.ProG.Service
                 serializer.WriteObject(s, myLibrary);
             }
         }
+
+
+        //private void GrantAccess(string fullPath)
+        //{
+        //    DirectoryInfo dInfo = new DirectoryInfo(fullPath);
+        //    DirectorySecurity dSecurity = dInfo.GetAccessControl();
+        //    dSecurity.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+        //    dInfo.SetAccessControl(dSecurity);
+        //}
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Les diagrammes de classes et de packages mettant en avant la persistance ainsi que notre de diagramme de classes se trouvent dans le fichier "diagrammes.mdj".
+
+Il faut exécuter le .exe en mode Administateur pour qe la sauvegarde fonctionne. 
