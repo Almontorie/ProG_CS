551be4346191d57edf5b828aed9a6879c0d8c762
Alexis <alexis.montorier@laposte.net>
2018-06-13 13:25:11 +0200

    suppression de code inutile en commentaire

--- Programmation/Almontorie.ProG.Servvice/XmlService.cs
+++ Programmation/Almontorie.ProG.Servvice/XmlService.cs
@@ -57,13 +57,5 @@ namespace Almontorie.ProG.Service
             }
         }
 
-
-        //private void GrantAccess(string fullPath)
-        //{
-        //    DirectoryInfo dInfo = new DirectoryInfo(fullPath);
-        //    DirectorySecurity dSecurity = dInfo.GetAccessControl();
-        //    dSecurity.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
-        //    dInfo.SetAccessControl(dSecurity);
-        //}
     }
 }
